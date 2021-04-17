4baef58875e1c9de0e86282ec8df7ea4877857f7
Yuliia Dubinina <yuliiadubinina@gmail.com>
2021-04-17 17:13:34 +0300

    first commit

--- projects/resto-lib-demo/src/app/app.component.spec.ts
+++ projects/resto-lib-demo/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'resto-lib'`, () => {
+  it(`should have as title 'resto-lib-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('resto-lib');
+    expect(app.title).toEqual('resto-lib-demo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to resto-lib!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to resto-lib-demo!');
   });
 });
